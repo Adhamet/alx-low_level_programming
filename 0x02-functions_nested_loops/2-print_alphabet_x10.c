1147926e701e017f524dc353cb633e546abac2f1
Adhamet <adhamessammetwally@gmail.com>
2023-02-21 21:31:35 +0200

    UPdated #2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 {
 	int line, ch;
 
-    for(line = 0; line < 10; line++)
-    {
-        for (ch = 'a'; ch <= 'z'; ++ch)
+	for (line = 0; line < 10; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
 			_putchar(ch);
 		_putchar('\n');
-    }
+	}
 }
