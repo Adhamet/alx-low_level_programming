bc6785f36fd7cf16440f09c0172c113dfda2c2df
Adhamet <adhamessammetwally@gmail.com>
2023-02-21 20:41:16 +0200

    Added #0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * 
+ * Description - print _putchar
+ * 
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+    char str[] = "_putchar";
+    
+    for(int ch = 0; ch < 8; ch++)
+        putchar(str[ch]);
+    putchar('\n');
+    
+    return (0);
+}
