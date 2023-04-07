d344d87a3c4058d57a4d9f35c56d7b043d5249ea
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-04-07 14:39:54 +0200

    Satisfied betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,14 +2,13 @@
 #include <string.h>
 
 /**
- * binary_to_unit - Converts binary number to unsigned integer.
+ * binary_to_uint - Converts binary number to unsigned integer.
  *
  * @b: pointer to string of characters.
  *
  * Return: the converted number, or 0 if there is one or more
  * characters in b that is not 0 or 1, or b is null
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
@@ -19,7 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len] != '\0'; len++);
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
 	i = len - 1;
 	for (; i >= 0; i--)
