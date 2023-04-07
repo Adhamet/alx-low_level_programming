9ec1931dc2b94080ec3f20d915c9b52e5dcdcdae
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-04-07 14:48:46 +0200

    Added task #2 Added function print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/** print_binary - Prints the binary of a decimal.
+ * 
+ * @n: number to print in binary
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
