32953dc590684e7594ca7424fc4ec5403febec5f
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-05-19 13:55:29 +0300

    Task #4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: pointer to the head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
